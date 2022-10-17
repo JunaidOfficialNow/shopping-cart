8634c29e6661d16159db079aadbffe91237e9fbf
Junaid <junaidofficialnow@gmail.com>
2022-10-18 00:46:05 +0530

    code formatted

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,49 +1,58 @@
 var db = require("../config/connection")
-var collections=require("../config/collections")
+var collections = require("../config/collections")
 var objectId = require('mongodb').ObjectID
 const bcrypt = require('bcrypt')
-module.exports={
-    addProduct:(product,callback)=>{
+module.exports = {
+    addProduct: (product, callback) => {
         product.price = parseInt(product.price)
-        db.get().collection(collections.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
-            var objectId=product._id
+        db.get().collection(collections.PRODUCT_COLLECTION).insertOne(product).then((data) => {
+            var objectId = product._id
             callback(objectId)
 
         })
-        
+
     },
-    getAllProducts:function(){
-        return new Promise(async(resolve,reject)=>{
-            let products =await db.get().collection(collections.PRODUCT_COLLECTION).find().toArray()
-            resolve (products)
+    getAllProducts: function () {
+        return new Promise(async (resolve, reject) => {
+            let products = await db.get().collection(collections.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
         })
     },
-    deleteProduct:function(proId){
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collections.PRODUCT_COLLECTION).deleteOne({_id:objectId(proId)}).then((data)=>{
+    deleteProduct: function (proId) {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collections.PRODUCT_COLLECTION).deleteOne({ _id: objectId(proId) }).then((data) => {
                 resolve(data)
             })
         })
 
     },
-     getProductDetails:function(ProId){
-        return new Promise((resolve,reject)=>{
-             db.get().collection(collections.PRODUCT_COLLECTION).find({_id:objectId(ProId)}).toArray().then((product)=>{
+    getProductDetails: function (ProId) {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collections.PRODUCT_COLLECTION).find({ _id: objectId(ProId) }).toArray().then((product) => {
                 resolve(product)
-             })
-                
             })
-        },
-     
-     editProduct:function(data,id){
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collections.PRODUCT_COLLECTION).update({_id:objectId(id)},{$set:{Name:data.Name,
-            category:data.category,price:data.price,description:data.description}}).then((data)=>{
+
+        })
+    },
+
+    editProduct: function (data, id) {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collections.PRODUCT_COLLECTION).update({ _id: objectId(id) },
+                {
+                    $set:
+                    {
+                        Name: data.Name,
+                        category: data.category,
+                        price: data.price,
+                        description: data.description
+                    }
+                }
+            ).then((data) => {
                 resolve(data)
             })
         })
-     },
-     doLogin: (adminData) => {
+    },
+    doLogin: (adminData) => {
         return new Promise(async (resolve, reject) => {
             let loginStatus = false
             let response = {}
@@ -69,19 +78,19 @@ module.exports={
 
         })
     },
-    getAllOrders:()=>{
-        return new Promise(async(resolve,reject)=>{
+    getAllOrders: () => {
+        return new Promise(async (resolve, reject) => {
             let orders = await db.get().collection(collections.ORDER_COLLECTION).find().toArray()
-                console.log(orders)
-                resolve(orders)
-            
-            
+            console.log(orders)
+            resolve(orders)
+
+
 
         })
     },
-    getCustomerDetails:(userId)=>{
-        return new Promise( async(resolve,reject)=>{
-            let user = await db.get().collection(collections.USER_COLLECTION).findOne({_id:objectId(userId)})
+    getCustomerDetails: (userId) => {
+        return new Promise(async (resolve, reject) => {
+            let user = await db.get().collection(collections.USER_COLLECTION).findOne({ _id: objectId(userId) })
             resolve(user)
         })
     },
@@ -118,26 +127,26 @@ module.exports={
             resolve(orderItems)
         })
     },
-    shipOrder:(orderId)=>{
-        return new Promise((resolve,reject)=>{
+    shipOrder: (orderId) => {
+        return new Promise((resolve, reject) => {
             db.get().collection(collections.ORDER_COLLECTION)
-            .updateOne({_id:objectId(orderId)},
-            {
-                $set:{
-                    status:'shipped'
-                }
-            }).then(()=>{
-                resolve()
-            })
+                .updateOne({ _id: objectId(orderId) },
+                    {
+                        $set: {
+                            status: 'shipped'
+                        }
+                    }).then(() => {
+                        resolve()
+                    })
         })
     },
-    getAllUsers:()=>{
-        return new Promise(async(resolve,reject)=>{
+    getAllUsers: () => {
+        return new Promise(async (resolve, reject) => {
             let users = await db.get().collection(collections.USER_COLLECTION).find().toArray()
             resolve(users)
         })
     }
-    
-    
-     
+
+
+
 }
